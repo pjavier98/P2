ee894fbb95458e2d574d7217f5dc9259bf0a80b2
Pedro Javier <35467783+pjavier98@users.noreply.github.com>
2018-07-31 13:59:52 -0300

    criação de lista null com nodes

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -1,124 +1,117 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*typedef struct linked_list  // definir lista
+typedef struct linked_list  // definir lista
 {
-    node *front;
-    node *back;
-    int size;
+	struct linked_list *front;
+	struct linked_list *back;
+	int size;
 } linked_list;
-*/
+
 typedef struct node // definir node
 {
-    int value;
-    struct node *next;
+	int value;
+	struct node *next;
 } node;
-
-/*linkedlist* create_linked_list() // criar nova lista
-{
-    linkedlist* list = (linkedlist*) malloc(sizeof(linkedlist));
-    list->size = 0;
-    list->front = NULL;
-    list->back = NULL;
-    return list;
-}*/
 
 node *create_node(int item) // criar node
 {
-    node* new_node = (node*) malloc(sizeof(node));
-    new_node -> value = item;
-    new_node -> next = NULL;
-    return new_node;
+	node* new_node = (node*) malloc(sizeof(node));
+	new_node -> value = item;
+	new_node -> next = NULL;
+	return new_node;
 }
 
 void print(node *head) // printar a lista
 {
-    while (head != NULL)
-    {
-        printf("%d\n", head -> value);
-        
-        head = head -> next;
-    }   
+	while (head != NULL)
+	{
+		printf("%d ", head -> value);
+
+		head = head -> next;
+	}   
+
+	printf("\n");
 }
 
-node* erase(node *head, int item)
+node* erase(node *head, int item) // apagar node
 {
     node *previous = NULL;      // anterior
     node *current = head;       // atual
 
     while (current != NULL && current -> value != item)
     {
-        previous = current;
-        current = current -> next;
+    	previous = current;
+    	current = current -> next;
     }
 
     if (current == NULL)
     {
-        return head;
+    	return head;
     }
 
     if (previous == NULL)
     {
-        head = current -> next;
+    	head = current -> next;
     }
 
     else
     {
-        previous -> next = current -> next;
+    	previous -> next = current -> next;
     }
 
     free(current);
     return head;
 }
 
-node* search(node *head, int value)
+node* search(node *head, int value) //procurar algum elemento na lista
 {
-    while (head != NULL)
-    {
-        if (head -> value == value)
-        {
-            return head;
-        }
+	while (head != NULL)
+	{
+		if (head -> value == value)
+		{
+			return head;
+		}
 
-        head = head -> next;
-    }
+		head = head -> next;
+	}
 
-    return NULL;
+	return NULL;
 }
 
-node *order_insert(node *head, int item)
+node *order_insert(node *head, int item) // inserir ordenadamente
 {
-    node *previous = NULL;
-    node *current = head;
-    node *new_node = create_node(item);
+	node *previous = NULL;
+	node *current = head;
+	node *new_node = create_node(item);
 
     if (head == NULL) // verifica se a lista nao tem nodes
     {
-        return new_node;
+    	return new_node;
     }
 
     if (head -> value >= item) // verifica se tem so um unico node na lista
     {
-        new_node -> next = head;
+    	new_node -> next = head;
 
-        return new_node;
+    	return new_node;
     }
 
     while (current != NULL && current -> value <= item) // 
     {
-        previous = current;
-        current = current -> next;
+    	previous = current;
+    	current = current -> next;
     }
     
     if (previous -> next == NULL) // verifica ate o ultimo item
     {
-        previous -> next = new_node;
-        new_node -> next = NULL;
+    	previous -> next = new_node;
+    	new_node -> next = NULL;
     }
 
     else 
     {
-        previous -> next = new_node;
+        previous -> next = new_node; // os ponteiros apontarem ate o new_node
 
         new_node -> next = current;
     }
@@ -126,65 +119,80 @@ node *order_insert(node *head, int item)
     return head;
 }
 
-node* insert_end(node *head, int item) 
+node* insert_end(node *head, int item) // inserir no final da lista
 {
-    node *new_node = create_node(item); 
-    new_node -> value = item;
-    new_node -> next = NULL;
+	node *new_node = create_node(item); 
+	new_node -> value = item;
+	new_node -> next = NULL;
 
-    if (head != NULL)
-    {
-        while (head -> next != NULL)
-        {
-            head = head -> next;
-        }
+	if (head != NULL)
+	{
+		while (head -> next != NULL)
+		{
+			head = head -> next;
+		}
 
-        head -> next = new_node;
+		head -> next = new_node;
 
-        return head;
-    }
-    else
-    {
-        return new_node;
-    }
+		return head;
+	}
+	else
+	{
+		return new_node;
+	}
 
 }
 
-node *insert_begin(node *head, int item)
+node *insert_begin(node *head, int item) // inserir no começo da lista
 {
-    node *new_node = create_node(item); 
-    new_node -> value = item;
-    new_node -> next = head; 
-    return new_node;
+	node *new_node = create_node(item); 
+	new_node -> value = item;
+	new_node -> next = head; 
+	return new_node;
 }
 
-node *linked_list()
+node *linked_list_node() // cria lista atraves de nodes
 {
-    return NULL;
+	return NULL;
 } 
 
-int is_empty(node *head)
+int is_empty(node *head) // verifica se a lista esta vazia
 {
-    return (head == NULL);
+	return (head == NULL);
+}
+
+linked_list* create_linked_list() // criar nova lista
+{
+	linked_list* list = (linked_list*) malloc(sizeof(linked_list));
+	list -> size = 0;
+	list -> front = NULL;
+	list -> back = NULL;
+	return list;
 }
 
 int main()
 {
-    //node *head = linked_list();
+	// is_empty(node *head);
 
-    //head = insert_begin(head, 15);
+	// node *head = linked_list_node();
 
-    //head = insert_end(head, 55);
-    
-    //head = erase(head, 7);
+	// head = insert_begin(head, 15);
 
-    //head = search(head, 55);
+	// head = insert_end(head, 55);
 
-    //head = order_insert(head, 1);
+	// head = erase(head, 7);
 
-    //is_empty(node *head);
-    
-    //print(head);
+	// head = search(head, 55);
 
-    return 0;
+	// for (int i = 0; i < 10; i += 1)
+	// {	
+	// 	int value;
+	// 	scanf("%d", &value);
+	// 	head = order_insert(head, value);
+	// 	print(head);
+	// }
+
+	// print(head);
+
+	return 0;
 }
