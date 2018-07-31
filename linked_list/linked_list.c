543c0f85ecd185ce378b7a038f93bba9f05ff74b
pjavier98 <pjpc@ic.ufal.br>
2018-07-30 23:03:05 -0300

    linked_list

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -1,13 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node
+typedef struct linked_list_  // definir lista
+{
+    Node *front;
+    Node *back;
+    int size;
+} linked_list;
+
+typedef struct node // definir node
 {
     int value;
     struct node *next;
 } node;
 
-void print(node *head)
+linkedlist* create_linked_list() // criar nova lista
+{
+    linkedlist* list = (linkedlist*) malloc(sizeof(linkedlist));
+    list->size = 0;
+    list->front = NULL;
+    list->back = NULL;
+    return list;
+}
+
+node *create_node(int item) // criar node
+{
+    node* new_node = (node*) malloc(sizeof(node));
+    new_node -> value = item;
+    new_node -> next = NULL;
+    return new_node;
+}
+
+void print(node *head) // printar a lista
 {
     while (head != NULL)
     {
@@ -17,11 +41,54 @@ void print(node *head)
     }   
 }
 
+node* erase(node *head, int item)
+{
+    node *previous = NULL;      // anterior
+    node *current = head;       // atual
+
+    while (current != NULL && current -> value != item)
+    {
+        previous = current;
+        current = current -> next;
+    }
+
+    if (current == NULL)
+    {
+        return head;
+    }
+
+    if (previous == NULL)
+    {
+        head = current -> next;
+    }
+
+    else
+    {
+        previous -> next = current -> next;
+    }
+
+    free(current);
+    return head;
+}
+
+node* search(node *head, int value)
+{
+    while (head != NULL)
+    {
+        if (head -> value == value)
+        {
+            return head;
+        }
+
+        head = head -> next;
+    }
+
+    return NULL;
+}
+
 node* insert_end(node *head, int item) 
 {
-    node *head = head;
-    node *new_node = (node*) malloc(sizeof(node)); 
-
+    node *new_node = create_node(item); 
     new_node -> value = item;
     new_node -> next = NULL;
 
@@ -43,10 +110,9 @@ node* insert_end(node *head, int item)
 
 }
 
-node* insert_begin(node *head, int item)
+node *insert_begin(node *head, int item)
 {
-    node *new_node = (node*) malloc(sizeof(node)); 
-
+    node *new_node = create_node(item); 
     new_node -> value = item;
     new_node -> next = head; 
     return new_node;
@@ -57,15 +123,26 @@ node *linked_list()
     return NULL;
 } 
 
+int is_empty(node *head)
+{
+    return (head == NULL);
+}
+
 int main()
 {
-    node *head = linked_list();
+    //node *head = linked_list();
 
-    head = insert_begin(head, 100);
+    //head = insert_begin(head, 100);
 
-    head = insert_end(head, 55);
+    //head = insert_end(head, 55);
     
-    print(head);
+    //head = erase(head, 7);
+
+    //head = search(head, 55);
+
+    //is_empty(node *head);
+    
+    //print(head);
 
     return 0;
 }
