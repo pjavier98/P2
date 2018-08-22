151193b2f01ca6ebcfef1aabe776af1c9ac176b4
Pedro Javier <35467783+pjavier98@users.noreply.github.com>
2018-08-22 17:09:31 -0300

    lista encadeada

--- list/linked_list/linked_list_head.c
+++ list/linked_list/linked_list_head.c
@@ -1,180 +1,130 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node // definir node
-{
-	int value;
-	struct node *next;
-} node;
+#define DEBUG(x, s) if(x)printf("%s\n", s);
 
-node *create_node(int item) // criar node
+typedef struct node
 {
-	node* new_node = (node*) malloc(sizeof(node));
-	new_node -> value = item;
-	new_node -> next = NULL;
+	int value; // pega o valor da entrada e faz o node atual apontar a ele
+	struct node *next; // faz o node apontar pro next
+}node;
+
+node *create_node(int item) // cria um node
+{
+	node *head = (node*) malloc(sizeof(node)); // aloca memoria para node
+	head->value = item; // adiciona o valor ao node
+	head->next = NULL; // faz o next do node apontar para NULL
+	return head; // retorna a cabeça
+}
+
+node *insert_end(node *head, int item) // insere no final
+{
+	node *new_node = create_node(item); // cria o node
+	node *aux = head; // faz o aux apontar para head tambem para nao perder o ponteiro da cabeça
+
+	if (head == NULL) // quando a lista nao tiver nenhum valor
+	{
+		return new_node;
+	}
+
+	while (aux->next != NULL) // quando ele nao for o final ele vai mudando pro proximo
+	{
+		aux=aux->next;
+	}
+
+	aux->next = new_node;
+	return head;	
+}
+
+node *insert_begin(node *head, int item) // insere no começo
+{
+	node *new_node = create_node(item); // cria o node
+	new_node->next = head; // faz o new node apontar para a cabeça
 	return new_node;
 }
 
-void print(node *head) // printar a lista
+void print(node *head) // printa a lista
+{
+	if (head == NULL) // enquanto a lista nao acabar ele printa
+	{
+		printf("\n");
+		return;
+	}
+	printf("%d ", head->value);
+	print(head->next); // recursao para incrementar o head
+}
+
+int search(node *head, int item) // procura algum elemento na lista
 {
 	if (head != NULL)
 	{
-		printf("%d ", head -> value);
-
-		print(head -> next);
-	}   
-}
-
-node* erase_node(node *head, int item) // apagar node
-{
-    node *previous = NULL;      // anterior
-    node *current = head;       // atual
-
-    while (current != NULL && current -> value != item)
-    {
-    	previous = current;
-    	current = current -> next;
-    }
-
-    if (current == NULL)
-    {
-    	return head;
-    }
-
-    if (previous == NULL)
-    {
-    	head = current -> next;
-    }
-
-    else
-    {
-    	previous -> next = current -> next;
-    }
-
-    free(current);
-    return head;
-}
-
-node* search(node *head, int value) //procurar algum elemento na lista
-{
-	while (head != NULL)
-	{
-		if (head -> value == value)
+		if (head->value == item)
 		{
-			return head;
+			return 1;
 		}
 
-		head = head -> next;
+		return search(head->next, item); // return permite a execução da parte abaixo dele do codigo
 	}
 
-	return NULL;
+	return 0;
 }
 
-node *order_insert(node *head, int item) // inserir ordenadamente
+node *erase(node *head, int item) // elimina um node de uma lista
 {
-	node *previous = NULL;
-	node *current = head;
-	node *new_node = create_node(item);
+	node *previous = NULL; // salva o anterior
+	node *current = head; // salva o atual
 
-    if (head == NULL) // verifica se a lista nao tem nodes
-    {
-    	return new_node;
-    }
+	if (current != NULL && current->value == item) // se o elemento a eliminar for a cabeça
+	{
+		head = current->next;
+		current->next = NULL;
+		free(current);
+		return head;
+	}
 
-    if (head -> value >= item) // verifica se tem so um unico node na lista
-    {
-    	new_node -> next = head;
+	while (current != NULL) // se o elemento a eliminar for diferente da cabeça
+	{
+		if (current->value == item)
+		{
+			previous->next = current->next;
+			free(current); // libera o node
+			return head;
+		}
+		previous = current; // vai incrementando o previous
+		current = current->next; // vai incrementando o current
+	}
 
-    	return new_node;
-    }
-
-    while (current != NULL && current -> value < item) // 
-    {
-    	previous = current;
-    	current = current -> next;
-    }
-    
-    if (previous -> next == NULL) // verifica ate o ultimo item
-    {
-    	previous -> next = new_node;
-    	new_node -> next = NULL;
-    }
-
-    else 
-    {
-        previous -> next = new_node; // os ponteiros apontarem ate o new_node
-
-        new_node -> next = current;
-    }
-
-    return head;
-}
-
-node* insert_end(node *head, int item) // inserir no final da lista
-{
-    node *new_node = create_node(item); 
-    node *aux = head; // para nao perder a referencia do ponteiro
-    
-    if (head != NULL)
-    {
-        while (head -> next != NULL)
-        {
-            head = head -> next;
-        }
-
-        head -> next = new_node;
-
-        return aux;
-    }
-
-    else
-    {
-        return new_node;
-    }
-
-}
-
-node *insert_begin(node *head, int item) // inserir no começo da lista
-{
-	node *new_node = create_node(item); 
-	new_node -> value = item;
-	new_node -> next = head; 
-	return new_node;
-}
-
-node *linked_list_node() // cria lista atraves de nodes
-{
-	return NULL;
-} 
-
-int is_empty(node *head) // verifica se a lista esta vazia
-{
-	return (head == NULL);
+	return head;
 }
 
 int main()
 {
-	// is_empty(node *head);
-
-	node *head = linked_list_node();
-
-	// head = insert_begin(head, 15);
-
-	// head = insert_end(head, 55);
-
-	// head = erase_node(head, 7);
-
-	// head = search(head, 55);
-
-	for (int i = 0; i < 4; i += 1)
-	{	
-		int value;
-		scanf("%d", &value);
-		head = order_insert(head, value);
-	}
-
+	node *head = NULL;
+	//DEBUG(0, "linha 102");
+	head = insert_begin(head, 3);
+	//DEBUG(0, "linha 104");
+	head = insert_begin(head, 7);
+	head = insert_begin(head, 9);
+	head = insert_end(head, 13);
+	head = insert_end(head, 17);
+	head = insert_end(head, 22);
 	print(head);
-    printf("\n");
-
+	//DEBUG(0, "linha 111");
+	head = erase(head, 9);
+	print(head);
+	head = erase(head, 13);
+	print(head);
+	head = erase(head, 22);
+	print(head);
+	if(search(head, 3)) {
+		printf("3 encontrado\n");
+	} else {
+		printf("3 nao encontrado\n");
+	}
+	if(search(head, 10000)) {
+		printf("10000 encontrado\n");
+	} else {
+		printf("100000 nao encontrado\n");
+	}
 	return 0;
 }
