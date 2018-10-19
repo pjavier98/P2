92d02f174bdc06cbb6d85175d905890c3074f6d4
pjavier98 <pjpc@ic.ufal.br>
2018-10-19 11:51:29 -0300

    upload

--- avl_abb/binary_tree/binary_tree.c
+++
@@ -1,9 +0,0 @@
-#include "binary_tree.h"
-
-binary_tree *create_binary_tree(lli item, binary_tree *left, binary_tree *right) {
-	binary_tree *new_bt = (binary_tree*) malloc(sizeof(binary_tree));
-	new_bt->item = item;
-	new_bt->height = 1;
-	new_bt->left = new_bt->right = NULL;
-	return new_bt;
-}
